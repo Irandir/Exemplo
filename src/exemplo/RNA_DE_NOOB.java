356356a68a739f631d698a0adf3be249641ce044
Irandir Onofre <irandironofre5@gmail.com>
2018-06-27 20:23:05 -0300

    2º commit

--- src/exemplo/RNA_DE_NOOB.java
+++ src/exemplo/RNA_DE_NOOB.java
@@ -1,5 +1,11 @@
 package exemplo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import org.encog.engine.network.activation.ActivationLinear;
 import org.encog.engine.network.activation.ActivationSigmoid;
 import org.encog.ml.data.MLData;
@@ -10,6 +16,8 @@ import org.encog.ml.train.MLTrain;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+
+import exemplos.RNAIris;
 
 public class RNA_DE_NOOB {
 
@@ -43,5 +51,36 @@ public class RNA_DE_NOOB {
 		}
 
 	}
+	public static ArrayList<Double[]> leituraDeArquivo(String nome) {
 
+		BufferedReader br = null;
+		ArrayList<Double[]> dados = new ArrayList<Double[]>();
+		try {
+			String path = RNA_DE_NOOB.class.getResource(nome).getPath();
+			br = new BufferedReader(new FileReader(path));
+			//int aux = 0;
+			String linha = null;
+			String linhaAux[] = null;
+			Double[] values = new Double[5];
+			while ((linha = br.readLine()) != null) {
+				linhaAux = null;
+				values = new Double[5];
+				linhaAux = linha.split("	");
+				for (int i = 0; i < values.length; i++) {
+					values[i] = Double.parseDouble(linhaAux[i]);
+				}
+				dados.add(values);
+			}
+
+			br.close();
+			return dados;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return null;
+	}
 }
